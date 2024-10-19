df2b870e9fa9df2b95b39cbed558f6169803a60a
Saika-m <165717332+saika-m@users.noreply.github.com>
2024-10-19 17:09:52 -0400

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"time"
 	"unsafe"
 
@@ -10,13 +11,14 @@ import (
 )
 
 const (
-	BNO055_ADDRESS   = 0x28
-	BMP280_ADDRESS   = 0x76
-	FILE_SHARE_READ  = 0x00000001
-	FILE_SHARE_WRITE = 0x00000002
-	OPEN_EXISTING    = 3
-	GENERIC_READ     = 0x80000000
-	GENERIC_WRITE    = 0x40000000
+	BNO055_ADDRESS     = 0x28
+	BMP280_ADDRESS     = 0x76
+	FILE_SHARE_READ    = 0x00000001
+	FILE_SHARE_WRITE   = 0x00000002
+	OPEN_EXISTING      = 3
+	GENERIC_READ       = 0x80000000
+	GENERIC_WRITE      = 0x40000000
+	SEA_LEVEL_PRESSURE = 1015.0 // hPa
 )
 
 var (
@@ -49,6 +51,11 @@ func main() {
 
 		if devicesFound(handle) {
 			fmt.Printf("Devices found on %s\n", deviceName)
+
+			// Initialize sensors
+			initBNO055(handle)
+			initBMP280(handle)
+
 			for {
 				// Read Euler angles from BNO055
 				heading, roll, pitch, err := readEulerAngles(handle)
@@ -63,7 +70,30 @@ func main() {
 				if err != nil {
 					log.Println("Error reading BMP280:", err)
 				} else {
-					fmt.Printf("Temperature: %.2f°C, Pressure: %.2f Pa\n", temp, pressure)
+					altitude := calcAltitude(SEA_LEVEL_PRESSURE, pressure)
+					fmt.Printf("Temperature: %.2f°C, Pressure: %.2f Pa, Altitude: %.2f m\n", temp, pressure, altitude)
+				}
+
+				// Read accelerometer, magnetometer, and gyroscope data
+				accX, accY, accZ, err := readAccelerometer(handle)
+				if err != nil {
+					log.Println("Error reading accelerometer:", err)
+				} else {
+					fmt.Printf("Acc: X=%.2f, Y=%.2f, Z=%.2f mg\n", accX, accY, accZ)
+				}
+
+				magX, magY, magZ, err := readMagnetometer(handle)
+				if err != nil {
+					log.Println("Error reading magnetometer:", err)
+				} else {
+					fmt.Printf("Mag: X=%.2f, Y=%.2f, Z=%.2f uT\n", magX, magY, magZ)
+				}
+
+				gyrX, gyrY, gyrZ, err := readGyroscope(handle)
+				if err != nil {
+					log.Println("Error reading gyroscope:", err)
+				} else {
+					fmt.Printf("Gyr: X=%.2f, Y=%.2f, Z=%.2f dps\n", gyrX, gyrY, gyrZ)
 				}
 
 				time.Sleep(100 * time.Millisecond)
@@ -122,6 +152,16 @@ func i2cTransfer(handle windows.Handle, addr uint16, writeData []byte, readData 
 	return nil
 }
 
+func initBNO055(handle windows.Handle) error {
+	// Set operation mode to NDOF
+	return i2cTransfer(handle, BNO055_ADDRESS, []byte{0x3D, 0x0C}, nil)
+}
+
+func initBMP280(handle windows.Handle) error {
+	// Set oversamplings for temperature and pressure and set mode to normal
+	return i2cTransfer(handle, BMP280_ADDRESS, []byte{0xF4, 0x57}, nil)
+}
+
 func readEulerAngles(handle windows.Handle) (float32, float32, float32, error) {
 	data := make([]byte, 6)
 	if err := i2cTransfer(handle, BNO055_ADDRESS, []byte{0x1A}, data); err != nil {
@@ -145,6 +185,49 @@ func readBMP280(handle windows.Handle) (float32, float32, error) {
 	temp := float32((uint32(data[3])<<12)|(uint32(data[4])<<4)|(uint32(data[5])>>4)) / 100.0
 
 	return temp, pressure, nil
+}
+
+func calcAltitude(seaLevel, pressure float32) float32 {
+	return 44330 * (1 - math.Pow(float64(pressure/100)/float64(seaLevel), 0.1903))
+}
+
+func readAccelerometer(handle windows.Handle) (float32, float32, float32, error) {
+	data := make([]byte, 6)
+	if err := i2cTransfer(handle, BNO055_ADDRESS, []byte{0x08}, data); err != nil {
+		return 0, 0, 0, err
+	}
+
+	x := float32(int16(data[0])|int16(data[1])<<8) / 100.0
+	y := float32(int16(data[2])|int16(data[3])<<8) / 100.0
+	z := float32(int16(data[4])|int16(data[5])<<8) / 100.0
+
+	return x, y, z, nil
+}
+
+func readMagnetometer(handle windows.Handle) (float32, float32, float32, error) {
+	data := make([]byte, 6)
+	if err := i2cTransfer(handle, BNO055_ADDRESS, []byte{0x0E}, data); err != nil {
+		return 0, 0, 0, err
+	}
+
+	x := float32(int16(data[0])|int16(data[1])<<8) / 16.0
+	y := float32(int16(data[2])|int16(data[3])<<8) / 16.0
+	z := float32(int16(data[4])|int16(data[5])<<8) / 16.0
+
+	return x, y, z, nil
+}
+
+func readGyroscope(handle windows.Handle) (float32, float32, float32, error) {
+	data := make([]byte, 6)
+	if err := i2cTransfer(handle, BNO055_ADDRESS, []byte{0x14}, data); err != nil {
+		return 0, 0, 0, err
+	}
+
+	x := float32(int16(data[0])|int16(data[1])<<8) / 16.0
+	y := float32(int16(data[2])|int16(data[3])<<8) / 16.0
+	z := float32(int16(data[4])|int16(data[5])<<8) / 16.0
+
+	return x, y, z, nil
 }
 
 func scanI2C(handle windows.Handle) {
