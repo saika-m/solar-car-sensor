b3d02c9d298c1d7c54a30eee0634a510f0e8c418
Saika-m <165717332+saika-m@users.noreply.github.com>
2024-10-19 13:26:52 -0400

    Update main.go

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"log"
 	"time"
-	"unsafe"
 
-	"golang.org/x/sys/windows"
+	"periph.io/x/conn/v3/i2c"
+	"periph.io/x/conn/v3/i2c/i2creg"
+	"periph.io/x/host/v3"
 )
 
 const (
@@ -14,44 +15,42 @@ const (
 	BMP280_ADDRESS = 0x76
 )
 
-var (
-	kernel32        = windows.NewLazySystemDLL("kernel32.dll")
-	createFile      = kernel32.NewProc("CreateFileW")
-	deviceIoControl = kernel32.NewProc("DeviceIoControl")
-)
-
-type I2C_TRANS_DATA struct {
-	Address uint16
-	Flags   uint16
-	Length  uint16
-	Buffer  uintptr
-}
-
 func main() {
-	// Open I2C device
-	handle, err := openI2CDevice()
+	// Initialize periph.io
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open default I2C bus
+	bus, err := i2creg.Open("")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer windows.CloseHandle(handle)
+	defer bus.Close()
+
+	// Create device for BNO055
+	bno055 := &i2c.Dev{Addr: BNO055_ADDRESS, Bus: bus}
+
+	// Create device for BMP280
+	bmp280 := &i2c.Dev{Addr: BMP280_ADDRESS, Bus: bus}
 
 	// Initialize sensors
-	if err := initBNO055(handle); err != nil {
+	if err := initBNO055(bno055); err != nil {
 		log.Fatal(err)
 	}
-	if err := initBMP280(handle); err != nil {
+	if err := initBMP280(bmp280); err != nil {
 		log.Fatal(err)
 	}
 
 	for {
 		// Read Euler angles from BNO055
-		heading, roll, pitch, err := readEulerAngles(handle)
+		heading, roll, pitch, err := readEulerAngles(bno055)
 		if err != nil {
 			log.Println("Error reading BNO055:", err)
 		}
 
 		// Read temperature and pressure from BMP280
-		temp, pressure, err := readBMP280(handle)
+		temp, pressure, err := readBMP280(bmp280)
 		if err != nil {
 			log.Println("Error reading BMP280:", err)
 		}
@@ -63,69 +62,27 @@ func main() {
 	}
 }
 
-func openI2CDevice() (windows.Handle, error) {
-	deviceName, err := windows.UTF16PtrFromString("\\\\.\\I2C1")
-	if err != nil {
-		return 0, err
-	}
-	handle, _, err := createFile.Call(
-		uintptr(unsafe.Pointer(deviceName)),
-		windows.GENERIC_READ|windows.GENERIC_WRITE,
-		0,
-		0,
-		windows.OPEN_EXISTING,
-		0,
-		0,
-	)
-	if handle == windows.InvalidHandle {
-		return 0, err
-	}
-	return windows.Handle(handle), nil
-}
-
-func i2cTransfer(handle windows.Handle, addr uint16, writeData []byte, readData []byte) error {
-	var bytesReturned uint32
-	writeBuffer := I2C_TRANS_DATA{
-		Address: addr,
-		Flags:   0,
-		Length:  uint16(len(writeData)),
-		Buffer:  uintptr(unsafe.Pointer(&writeData[0])),
-	}
-	readBuffer := I2C_TRANS_DATA{
-		Address: addr,
-		Flags:   1,
-		Length:  uint16(len(readData)),
-		Buffer:  uintptr(unsafe.Pointer(&readData[0])),
-	}
-	_, _, err := deviceIoControl.Call(
-		uintptr(handle),
-		0x22C004, // IOCTL_I2C_TRANSFER
-		uintptr(unsafe.Pointer(&writeBuffer)),
-		unsafe.Sizeof(writeBuffer),
-		uintptr(unsafe.Pointer(&readBuffer)),
-		unsafe.Sizeof(readBuffer),
-		uintptr(unsafe.Pointer(&bytesReturned)),
-		0,
-	)
-	if err != windows.ERROR_SUCCESS {
+func initBNO055(dev *i2c.Dev) error {
+	// Set operation mode to NDOF
+	if err := dev.Tx([]byte{0x3D, 0x0C}, nil); err != nil {
 		return err
 	}
+	time.Sleep(20 * time.Millisecond)
 	return nil
 }
 
-func initBNO055(handle windows.Handle) error {
-	// Set operation mode to NDOF
-	return i2cTransfer(handle, BNO055_ADDRESS, []byte{0x3D, 0x0C}, nil)
-}
-
-func initBMP280(handle windows.Handle) error {
+func initBMP280(dev *i2c.Dev) error {
 	// Set oversample for pressure and temperature and set mode to normal
-	return i2cTransfer(handle, BMP280_ADDRESS, []byte{0xF4, 0x57}, nil)
+	if err := dev.Tx([]byte{0xF4, 0x57}, nil); err != nil {
+		return err
+	}
+	time.Sleep(20 * time.Millisecond)
+	return nil
 }
 
-func readEulerAngles(handle windows.Handle) (float64, float64, float64, error) {
+func readEulerAngles(dev *i2c.Dev) (float64, float64, float64, error) {
 	data := make([]byte, 6)
-	if err := i2cTransfer(handle, BNO055_ADDRESS, []byte{0x1A}, data); err != nil {
+	if err := dev.Tx([]byte{0x1A}, data); err != nil {
 		return 0, 0, 0, err
 	}
 
@@ -136,9 +93,9 @@ func readEulerAngles(handle windows.Handle) (float64, float64, float64, error) {
 	return heading, roll, pitch, nil
 }
 
-func readBMP280(handle windows.Handle) (float64, float64, error) {
+func readBMP280(dev *i2c.Dev) (float64, float64, error) {
 	data := make([]byte, 6)
-	if err := i2cTransfer(handle, BMP280_ADDRESS, []byte{0xF7}, data); err != nil {
+	if err := dev.Tx([]byte{0xF7}, data); err != nil {
 		return 0, 0, err
 	}
 
