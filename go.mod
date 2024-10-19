93e16eefb4bb08b8435c1d0b97218d43c17691e5
Saika-m <165717332+saika-m@users.noreply.github.com>
2024-10-19 13:32:07 -0400

    fix

--- go.mod
+++ go.mod
@@ -1,5 +1,20 @@
 module sensor
 
-go 1.16
+go 1.22
 
-require golang.org/x/sys v0.26.0
+toolchain go1.22.5
+
+require (
+	golang.org/x/sys v0.26.0
+	periph.io/x/conn/v3 v3.7.1
+)
+
+require (
+	github.com/sigurn/crc16 v0.0.0-20240131213347-83fcde1e29d1 // indirect
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+)
+
+require (
+	github.com/jonboulle/clockwork v0.4.0 // indirect
+	periph.io/x/host/v3 v3.8.2
+)
--- main.go
+++ main.go
@@ -5,102 +5,83 @@ import (
 	"log"
 	"time"
 
-	"periph.io/x/conn/v3/i2c"
-	"periph.io/x/conn/v3/i2c/i2creg"
-	"periph.io/x/host/v3"
-)
-
-const (
-	BNO055_ADDRESS = 0x28
-	BMP280_ADDRESS = 0x76
+	"github.com/sigurn/crc16"
+	"github.com/tarm/serial"
 )
 
 func main() {
-	// Initialize periph.io
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Open default I2C bus
-	bus, err := i2creg.Open("")
+	c := &serial.Config{Name: "COM3", Baud: 115200}
+	s, err := serial.OpenPort(c)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer bus.Close()
-
-	// Create device for BNO055
-	bno055 := &i2c.Dev{Addr: BNO055_ADDRESS, Bus: bus}
-
-	// Create device for BMP280
-	bmp280 := &i2c.Dev{Addr: BMP280_ADDRESS, Bus: bus}
-
-	// Initialize sensors
-	if err := initBNO055(bno055); err != nil {
-		log.Fatal(err)
-	}
-	if err := initBMP280(bmp280); err != nil {
 		log.Fatal(err)
 	}
 
 	for {
-		// Read Euler angles from BNO055
-		heading, roll, pitch, err := readEulerAngles(bno055)
+		// Read Euler angles
+		heading, roll, pitch, err := readEulerAngles(s)
 		if err != nil {
-			log.Println("Error reading BNO055:", err)
+			log.Println("Error reading Euler angles:", err)
+		} else {
+			fmt.Printf("Heading: %.2f, Roll: %.2f, Pitch: %.2f\n", heading, roll, pitch)
 		}
 
-		// Read temperature and pressure from BMP280
-		temp, pressure, err := readBMP280(bmp280)
+		// Read temperature and pressure
+		temp, pressure, err := readTempPressure(s)
 		if err != nil {
-			log.Println("Error reading BMP280:", err)
+			log.Println("Error reading temperature and pressure:", err)
+		} else {
+			fmt.Printf("Temperature: %.2f°C, Pressure: %.2f Pa\n", temp, pressure)
 		}
-
-		fmt.Printf("Heading: %.2f, Roll: %.2f, Pitch: %.2f, Temp: %.2f°C, Pressure: %.2f Pa\n",
-			heading, roll, pitch, temp, pressure)
 
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
-func initBNO055(dev *i2c.Dev) error {
-	// Set operation mode to NDOF
-	if err := dev.Tx([]byte{0x3D, 0x0C}, nil); err != nil {
-		return err
-	}
-	time.Sleep(20 * time.Millisecond)
-	return nil
-}
+func readEulerAngles(s *serial.Port) (float32, float32, float32, error) {
+	cmd := []byte{0xA5, 0x45, 0xAA, 0x01, 0x00, 0x00}
+	table := crc16.MakeTable(crc16.CRC16_MODBUS)
+	crc := crc16.Checksum(cmd[:4], table)
+	cmd[4] = byte(crc & 0xFF)
+	cmd[5] = byte((crc >> 8) & 0xFF)
 
-func initBMP280(dev *i2c.Dev) error {
-	// Set oversample for pressure and temperature and set mode to normal
-	if err := dev.Tx([]byte{0xF4, 0x57}, nil); err != nil {
-		return err
-	}
-	time.Sleep(20 * time.Millisecond)
-	return nil
-}
-
-func readEulerAngles(dev *i2c.Dev) (float64, float64, float64, error) {
-	data := make([]byte, 6)
-	if err := dev.Tx([]byte{0x1A}, data); err != nil {
+	_, err := s.Write(cmd)
+	if err != nil {
 		return 0, 0, 0, err
 	}
 
-	heading := float64(int16(data[0])|int16(data[1])<<8) / 16.0
-	roll := float64(int16(data[2])|int16(data[3])<<8) / 16.0
-	pitch := float64(int16(data[4])|int16(data[5])<<8) / 16.0
+	buf := make([]byte, 11)
+	_, err = s.Read(buf)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	heading := float32(int16(buf[3])<<8|int16(buf[4])) / 100.0
+	roll := float32(int16(buf[5])<<8|int16(buf[6])) / 100.0
+	pitch := float32(int16(buf[7])<<8|int16(buf[8])) / 100.0
 
 	return heading, roll, pitch, nil
 }
 
-func readBMP280(dev *i2c.Dev) (float64, float64, error) {
-	data := make([]byte, 6)
-	if err := dev.Tx([]byte{0xF7}, data); err != nil {
+func readTempPressure(s *serial.Port) (float32, float32, error) {
+	cmd := []byte{0xA5, 0x45, 0xAA, 0x02, 0x00, 0x00}
+	table := crc16.MakeTable(crc16.CRC16_MODBUS)
+	crc := crc16.Checksum(cmd[:4], table)
+	cmd[4] = byte(crc & 0xFF)
+	cmd[5] = byte((crc >> 8) & 0xFF)
+
+	_, err := s.Write(cmd)
+	if err != nil {
 		return 0, 0, err
 	}
 
-	pressure := float64((uint32(data[0])<<12)|(uint32(data[1])<<4)|(uint32(data[2])>>4)) / 100.0
-	temp := float64((uint32(data[3])<<12)|(uint32(data[4])<<4)|(uint32(data[5])>>4)) / 100.0
+	buf := make([]byte, 11)
+	_, err = s.Read(buf)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	temp := float32(int16(buf[3])<<8|int16(buf[4])) / 100.0
+	pressure := float32(int32(buf[5])<<16|int32(buf[6])<<8|int32(buf[7])) / 100.0
 
 	return temp, pressure, nil
 }
